fd26ad6911c9b01d3e7361b71d0179982f8f5338
Ali Sikander <26100074@lums.edu.pk>
2026-08-07 02:30:35 +0500

    fix: preserve Link header for empty res.links input

--- test/res.links.js
+++ test/res.links.js
@@ -1,10 +1,41 @@
 'use strict'
 
+var assert = require('node:assert');
 var express = require('..');
 var request = require('supertest');
+var utils = require('./support/utils');
 
 describe('res', function(){
   describe('.links(obj)', function(){
+    it('should not set Link header field for an empty object', function (done) {
+      var app = express();
+
+      app.use(function (req, res) {
+        assert.strictEqual(res.links({}), res);
+        res.end();
+      });
+
+      request(app)
+      .get('/')
+      .expect(utils.shouldNotHaveHeader('Link'))
+      .expect(200, done);
+    })
+
+    it('should preserve Link header field for an empty object', function (done) {
+      var app = express();
+
+      app.use(function (req, res) {
+        res.set('Link', '<http://api.example.com/users?page=1>; rel="prev"');
+        res.links({});
+        res.end();
+      });
+
+      request(app)
+      .get('/')
+      .expect('Link', '<http://api.example.com/users?page=1>; rel="prev"')
+      .expect(200, done);
+    })
+
     it('should set Link header field', function (done) {
       var app = express();
 
